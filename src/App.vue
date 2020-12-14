89559593fd3d235856ec6626eaea32ddc30a173e
Nathalia Garcia <nathalia.garcia@prodigious.com>
2020-12-14 12:03:07 -0300

    app improvements

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+
+export default {
+}
+</script>
+
 <style lang="stylus">
+
+body
+  margin 0
+
 #app
+  display flex
+  flex-direction column
+  align-items center
+  justify-content center
+  background #f6f6f6
+  width 100%
+  min-height 100vh
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
-  color #2c3e50
-  margin-top 60px
+  color #111
+
 </style>
