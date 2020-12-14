edd614bd150cb0b9396cb5b2e488680fd314a4d6
Nathalia Garcia <nathalia.garcia@prodigious.com>
2020-12-14 12:00:16 -0300

    configures public and private routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Cadastro from '../views/Cadastro.vue'
+import firebase from "firebase/app";
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
+const router = new VueRouter ({
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '*',
+      redirect: '/login'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/cadastro',
+      name: 'Cadastro',
+      component: Cadastro
+    },
+  ],
+})
+
+router.beforeEach( (to, from, next) => {
+  const user = firebase.auth().currentUser
+  const requiresAuth = to.matched.some( record => record.meta.requiresAuth)
+
+  if (requiresAuth && !user) next('login')
+  else if (!requiresAuth && user) next('home')
+  else next()
 })
 
 export default router
